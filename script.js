ec9a285d588ab88cbae4b576dcf419983d2be2ce
VadimZhuckow <apexknight@yandex.ru>
2022-01-20 11:44:25 +0300

    nav

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// let app = document.querySelector(".weather"); //  блок для того чтобы выводить погу 
+// let app = document.querySelector(".weather"); //  блок для того чтобы выводить погу
 let city = document.querySelector(".city"); // блок для вывода города
 
 // // запрос получает данные о погоде только по городу
@@ -8,14 +8,12 @@ let city = document.querySelector(".city"); // блок для вывода го
 //     .then((data) => data.json())
 //     .then((data) => (app.textContent = Math.round(data.main.temp - 273.15))); // очень не понятно зачем??
 
-
 // //  запрос определят город
 // fetch(
 //         "https://geo.ipify.org/api/v2/country?apiKey=at_Hw9HqpTOjpkwOCGmpkYCO3KKYHXUt&ipAddress=178.66.158.67"
 //     )
 //     .then((log) => log.json())
 //     .then((log) => (city.innerHTML = log.location.region));
-
 
 // // функция определяет местоположение пользователя
 // navigator.geolocation.getCurrentPosition(function(position) {
@@ -42,41 +40,68 @@ let city = document.querySelector(".city"); // блок для вывода го
 // }
 
 // navigator.geolocation.getCurrentPosition(success, error, options);
-
+// function test(){
+//   return navigator;
+// }
+// test();
 // console.log('hello');
 let x = 0;
 let y = 0;
 var options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
 };
 // при согласие пользовтаеля дать геоданные, выполняется эта функция
+
+ async function ip(){ // функция получения ip
+  //  let res = await fetch('https://api.db-ip.com/v2/free/self');
+  //  let data = res.json();
+  //  return data
+  //  console.log(c)
+  //&ipAddress=${x}`
+  
+   fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_Hw9HqpTOjpkwOCGmpkYCO3KKYHXUt`)
+        .then((city) => city.json())
+        .then((city) => (console.log(city)));
+    }
+  
+ 
+ 
+
+
+
 function success(pos) {
-    var crd = pos.coords;
+  var crd = pos.coords;
 
-    let x = crd.latitude;
-    let y = crd.longitude;
-    console.log(`Широта: ${x}`);
-    console.log(`Долгота: ${y}`);
-    fetch(
-            // "http://api.openweathermap.org/data/2.5/weather?q=Saint Petersburg,RU&appid=51d1d0a1921b759daf26dad3f5f28daa&units=metric"
-            `https://api.openweathermap.org/data/2.5/onecall?lat=${x}&lon=${y}&exclude={part}&appid=51d1d0a1921b759daf26dad3f5f28daa&units=metric`
-        )
-        .then((data) => data.json())
-        .then((data) => {
-            console.log(data.current.temp) // app можно вывести данные сюда
-        });
-
-};
-
+  let x = crd.latitude;
+  let y = crd.longitude;
+  console.log(`Широта: ${x}`);
+  console.log(`Долгота: ${y}`);
+  fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${x}&lon=${y}&exclude={part}&appid=51d1d0a1921b759daf26dad3f5f28daa&units=metric`
+  )
+    .then((data) => data.json())
+    .then((data) => {
+      console.log(data.current.temp); // app можно вывести данные сюда
+    });
+    
+   
+// ip();  
+fetch(
+"https://geo.ipify.org/api/v2/country?apiKey=at_Hw9HqpTOjpkwOCGmpkYCO3KKYHXUt&ipAddress=178.66.158.67")
+//     )
+//     .then((log) => log.json())
+//     .then((log) => (city.innerHTML = log.location.region));
+}
 
 // если пользователь не дает свое согласие то следуем тому скрипту который ниже
 function error(err) {
-    console.warn('пользователь не дал сове согласие')
-        // получить ip пользователя
-        // узнать город пользователя по его ip
-        // зная город мы можем вычислить погоду
-};
+  console.warn("пользователь не дал сове согласие");
+  ip() ; // получение ip при запрете
+  // получить ip пользователя
+  // узнать город пользователя по его ip
+  // зная город мы можем вычислить погоду
+}
 
 navigator.geolocation.getCurrentPosition(success, error, options);
